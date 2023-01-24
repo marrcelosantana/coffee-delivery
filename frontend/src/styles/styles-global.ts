5639d518e48934ff506e64fbad39c605aa29450c
marrcelosantana <marcelosantana@alu.ufc.br>
2023-01-24 17:51:04 -0300

    [Coffee-Delivery] - First commmit

---
+++ frontend/src/styles/styles-global.ts
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: "Baloo 2", sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+
+  
+@media (max-width: 1080px) {
+  html {
+    font-size: 93.75%;
+  }
+}
+
+@media (max-width: 720px) {
+  html {
+    font-size: 87.5%;
+  }
+}
+
+button {
+  cursor: pointer;
+}
+
+[disabled] {
+  opacity: 0.6;
+  cursor: not-allowed;
+}
+
+`;
