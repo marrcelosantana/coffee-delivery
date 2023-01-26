d4a7ed4a09dafb941aaaa01f8ece50d0077af371
marcelosantana <marcelosantana@alu.ufc.br>
2023-01-25 23:41:45 -0300

    [Coffee-Delivery] - Finish layout

--- frontend/src/pages/Success/index.tsx
+++ frontend/src/pages/Success/index.tsx
@@ -1,3 +1,62 @@
+import { MapPin, Timer, CurrencyDollar } from "phosphor-react";
+import successImg from "../../assets/illustration.png";
+
+import {
+  IconContainer,
+  Info,
+  OrderInfo,
+  SuccessContainer,
+  SuccessContent,
+} from "./styles";
+
 export function Success() {
-  return <h1>Hello World</h1>;
+  return (
+    <SuccessContainer>
+      <div className="title">
+        <h1>Uhu! Pedido confirmado.</h1>
+        <p>Agora é só aguardar que logo o café chegará até você.</p>
+      </div>
+
+      <SuccessContent>
+        <OrderInfo>
+          <Info>
+            <IconContainer backgroundColor="purple">
+              <MapPin size={16} weight="fill" />
+            </IconContainer>
+            <div className="description">
+              <p>
+                Entrega em{" "}
+                <span style={{ fontWeight: "bold" }}>
+                  Rua João Daniel Martinelli, 102
+                </span>{" "}
+              </p>
+              <p style={{ fontWeight: "400" }}>Farrapos - Porto Alegre, RS</p>
+            </div>
+          </Info>
+
+          <Info>
+            <IconContainer backgroundColor="yellow">
+              <Timer size={16} weight="fill" />
+            </IconContainer>
+            <div className="description">
+              <p>Previsão de entrega:</p>
+              <p>20 min - 30 min</p>
+            </div>
+          </Info>
+
+          <Info>
+            <IconContainer backgroundColor="darkYellow">
+              <CurrencyDollar size={16} weight="fill" />
+            </IconContainer>
+            <div className="description">
+              <p>Pagamento na entrega</p>
+              <p>Cartão de crédito</p>
+            </div>
+          </Info>
+        </OrderInfo>
+
+        <img src={successImg} alt="" />
+      </SuccessContent>
+    </SuccessContainer>
+  );
 }
--- frontend/src/pages/Success/styles.ts
+++ frontend/src/pages/Success/styles.ts
@@ -1,1 +1,73 @@
 import styled from "styled-components";
+
+export const SuccessContainer = styled.div`
+  margin-top: 3rem;
+  display: flex;
+  flex-direction: column;
+
+  .title {
+    h1 {
+      color: ${(props) => props.theme["yellow-dark"]};
+      font-size: 2rem;
+      font-family: "Baloo 2";
+    }
+
+    p {
+      font-size: 1.25rem;
+      line-height: 1.625;
+    }
+  }
+`;
+
+export const SuccessContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const OrderInfo = styled.div`
+  border: 1px solid ${(props) => props.theme["purple-normal"]};
+  padding: 2.5rem;
+  border-radius: 6px 36px;
+  margin-top: 2rem;
+
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+export const Info = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.875rem;
+
+  .description {
+    p + p {
+      font-weight: bold;
+    }
+  }
+`;
+
+const BACKGROUND_ITEM_COLORS = {
+  yellow: "yellow-normal",
+  darkYellow: "yellow-dark",
+  purple: "purple-normal",
+} as const;
+
+interface BackgroundItemProps {
+  backgroundColor: keyof typeof BACKGROUND_ITEM_COLORS;
+}
+
+export const IconContainer = styled.div<BackgroundItemProps>`
+  width: 2rem;
+  height: 2rem;
+  border-radius: 50%;
+  color: white;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background: ${(props) =>
+    props.theme[BACKGROUND_ITEM_COLORS[props.backgroundColor]]};
+`;
